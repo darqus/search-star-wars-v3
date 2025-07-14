286da3cd14afc56f3601510754b6a0979f85fb31
darqus <voladores@inbox.ru>
2025-07-14 23:08:07 +0300

    update env.ts

--- src/config/env.ts
+++ src/config/env.ts
@@ -7,10 +7,10 @@ type EnvConfig = {
 }
 
 export const env: EnvConfig = {
-  API_BASE_URL: import.meta.env.VITE_APP_API_BASE_URL || '',
-  APP_NAME: import.meta.env.VITE_APP_NAME || 'Star Wars Search',
-  APP_VERSION: import.meta.env.VITE_APP_VERSION || '1.0.0',
-  BUILD_DATE: import.meta.env.VITE_APP_BUILD_DATE || new Date().toISOString().split('T')[0],
+  API_BASE_URL: import.meta.env.VITE_APP_API_BASE_URL ?? '',
+  APP_NAME: import.meta.env.VITE_APP_NAME ?? 'Star Wars Search',
+  APP_VERSION: import.meta.env.VITE_APP_VERSION ?? '1.0.0',
+  BUILD_DATE: import.meta.env.VITE_APP_BUILD_DATE ?? new Date().toISOString().split('T')[0],
   IS_PRODUCTION: import.meta.env.PROD,
 }
 
